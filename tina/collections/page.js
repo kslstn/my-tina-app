45a5d3114f86516485bff0d29a0c798da1dc36af
Koos Looijesteijn <@kslstn>
2024-09-17 21:16:09 +0200

    Add template array for 'Page content' collection

--- tina/collections/page.js
+++ tina/collections/page.js
@@ -6,20 +6,31 @@ export default {
   name: "page",
   path: "content/page",
   format: "mdx",
-  fields: [
-    {
-      name: "body",
-      label: "Main Content",
-      type: "rich-text",
-      isBody: true,
-    },
-  ],
   ui: {
-    router: ({ document }) => {
-      if (document._sys.filename === "home") {
-        return `/`;
-      }
-      return undefined;
+    allowedActions: {
+      createNestedFolder: true,
     },
   },
+  templates: [
+    {
+      label: 'Page template 1',
+      name: 'template1',
+      fields: [
+        {
+          name: "body",
+          label: "Main Content",
+          type: "rich-text",
+          isBody: true,
+        },
+      ],
+      ui: {
+        router: ({ document }) => {
+          if (document._sys.filename === "home") {
+            return `/`;
+          }
+          return undefined;
+        },
+      },
+    }
+  ]
 };
